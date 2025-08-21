35ebba3d7ee11e4dc73bd8d6ccfa1e5045542fc6
riet <loli_520@126.com>
2025-08-21 13:38:43 +0800

    sqlite Option

--- db/sqlite/sqlite.go
+++ db/sqlite/sqlite.go
@@ -2,6 +2,8 @@ package sqlite
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -9,10 +11,53 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func NewDB(path string) (*sql.DB, error) {
-	return sql.Open("sqlite3", path)
+type Option func(map[string]string)
+
+// WithAuth go build -tags "sqlite_userauth" add such tags when compile to enable user auth
+func WithAuth(username, password string) Option {
+	return func(m map[string]string) {
+		m["_auth"] = ""
+		m["_auth_user"] = username
+		m["_auth_pass"] = password
+	}
 }
 
-func NewGormDB(path string) (*gorm.DB, error) {
-	return gorm.Open(sqlite.Open(path))
+func WithInMemory() Option {
+	return func(m map[string]string) {
+		m["mode"] = "memory"
+	}
+}
+
+func WithCacheShared() Option {
+	return func(m map[string]string) {
+		m["cache"] = "shared"
+	}
+}
+
+func formatDSN(name string, options ...Option) string {
+	dsn := fmt.Sprintf("file:%s", name)
+	if len(options) == 0 {
+		return dsn
+	}
+	params := make(map[string]string)
+	for _, option := range options {
+		option(params)
+	}
+	var param []string
+	for k, v := range params {
+		if v != "" {
+			param = append(param, fmt.Sprintf("%s=%s", k, v))
+		} else {
+			param = append(param, k)
+		}
+	}
+	return fmt.Sprintf("%s?%s", dsn, strings.Join(param, "&"))
+}
+
+func NewDB(name string, options ...Option) (*sql.DB, error) {
+	return sql.Open("sqlite3", formatDSN(name, options...))
+}
+
+func NewGormDB(name string, options ...Option) (*gorm.DB, error) {
+	return gorm.Open(sqlite.Open(formatDSN(name, options...)))
 }
